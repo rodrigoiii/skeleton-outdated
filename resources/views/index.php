2f5a25ce5bf47339f462bfe08bae3c828c519bab
rodz3rd2 <rodrigogalura3rd@gmail.com>
2017-11-29 11:41:59 +0800

    add sample for implementing css and js

--- resources/views/index.php
+++ resources/views/index.php
@@ -2,9 +2,12 @@
 <html>
 <head>
 	<title>Test View</title>
+	<!-- <link rel="stylesheet" type="text/css" href="/css/index.css"> -->
 </head>
 <body>
 	<h3>Test View</h3>
 	<p>Hello World</p>
+
+	<!-- <script type="text/javascript" src="/js/index.js"></script> -->
 </body>
 </html>
