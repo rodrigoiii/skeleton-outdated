341dfbc569778175407cfa95ae624b01d087ecf3
rodz3rd2 <rodrigogalura3rd@gmail.com>
2017-12-14 13:52:20 +0800

    add base middleware

---
+++ app/Http/Middlewares/Middleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middlewares;
+
+class Middleware
+{
+	protected $container;
+
+	public function __construct($container)
+	{
+		$this->container = $container;
+	}
+
+	public function __get($property)
+	{
+		if (isset($this->container->{$property}))
+		{
+			return $this->container->{$property};
+		}
+	}
+}
