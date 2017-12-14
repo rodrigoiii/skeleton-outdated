c5c8106c20b81c0b753079c01f68021a5676076e
rodz3rd2 <rodrigogalura3rd@gmail.com>
2017-12-14 16:03:38 +0800

    add monolog

---
+++ core/Log.php
@@ -0,0 +1,18 @@
+<?php
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+class Log
+{
+	// public static function debug($message)
+	public static function write($method, $message)
+	{
+		$settings = config('monolog');
+
+		$log = new Logger($settings['name']);
+		$log->pushHandler(new StreamHandler($settings['path']), $settings['level']);
+
+		$log->$method($message);
+	}
+}
