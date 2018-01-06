a5e488d4e60f689435a685e69ebabb45a34257cc
rodz3rd <rodrigogalura3rd@gmail.com>
2018-01-06 23:35:53 +0800

    add web_mode in config

--- config/app.php
+++ config/app.php
@@ -4,6 +4,7 @@ return [
 	'name'      => _env('APP_NAME', "App Name"),
 	'namespace' => _env('APP_NAMESPACE', "App"),
 	'key'       => _env('APP_KEY'),
+	'mode' 		=> _env('WEB_MODE'),
 
 	'cache' => false,
 
