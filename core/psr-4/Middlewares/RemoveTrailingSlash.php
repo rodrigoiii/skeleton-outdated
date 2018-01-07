65030076d4ed24d8841dcd61c960173473e43dcb
rodz3rd <rodrigogalura3rd@gmail.com>
2018-01-07 18:31:35 +0800

    collapse unnecessary code

--- core/psr-4/Middlewares/RemoveTrailingSlash.php
+++ core/psr-4/Middlewares/RemoveTrailingSlash.php
@@ -13,12 +13,11 @@ class RemoveTrailingSlash extends Middleware
 		    // to their non-trailing counterpart
 		    $uri = $uri->withPath(substr($path, 0, -1));
 
-		    if($request->getMethod() == 'GET') {
+		    if ($request->getMethod() == 'GET') {
 		        return $response->withRedirect((string)$uri, 301);
 		    }
-		    else {
-		        return $next($request->withUri($uri), $response);
-		    }
+
+		    return $next($request->withUri($uri), $response);
 		}
 
 		return $next($request, $response);
