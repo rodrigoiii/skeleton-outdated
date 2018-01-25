ca84114149e7ce9657b7fa41fb6baa5feb61af4c
rodz3rd <rodrigogalura3rd@gmail.com>
2018-01-25 19:54:08 +0800

    add utilities class and include illuminate/pagination in framework

---
+++ core/psr-4/Utilities/Flash.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Utilities;
+
+use Slim\Flash\Messages;
+
+class Flash
+{
+    private $flash;
+
+    public function __construct()
+    {
+        $this->flash = new Messages;
+    }
+
+    public function addMessage($is_passed, array $pass_message, array $fail_message)
+    {
+        if ($is_passed)
+        {
+            $key = key($pass_message);
+            $this->flash->addMessage($key, $pass_message[$key]);
+        }
+        else
+        {
+            $key = key($fail_message);
+            $this->flash->addMessage($key, $fail_message[$key]);
+        }
+    }
+}
---
+++ core/psr-4/Utilities/Time.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Utilities;
+
+class Time
+{
+    const SECOND = 1;
+    const MINUTE = 60;
+    const HOUR = 60 * 60;
+    const DAY = 60 * 60 * 24;
+}
