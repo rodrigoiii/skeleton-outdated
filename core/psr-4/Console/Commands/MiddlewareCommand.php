49791c0ff2e385e84e46c1bb5049d74bbaccef59
rodz3rd <rodrigogalura3rd@gmail.com>
2018-01-27 10:08:37 +0800

    enable to add subfolder when making middleware

--- core/psr-4/Console/Commands/MiddlewareCommand.php
+++ core/psr-4/Console/Commands/MiddlewareCommand.php
@@ -33,14 +33,31 @@ class MiddlewareCommand extends BaseCommand
     {
         $middleware = $input->getArgument('middleware');
 
+        $pre_middleware_path = app_path("Http/Middlewares");
+        $sub_directories = "";
+
+        // have directory
+        if (strpos($middleware, "/"))
+        {
+            $explode_middleware = explode("/", $middleware);
+            $middleware = array_pop($explode_middleware);
+
+            $pre_middleware_path = app_path("Http/Middlewares/" . implode("/", $explode_middleware));
+            $sub_directories = "\\" . implode("\\", $explode_middleware);
+
+            // create directory
+            if (!file_exists($pre_middleware_path))
+                mkdir($pre_middleware_path, 0755, true);
+        }
+
         try {
             if (!ctype_upper($middleware[0]))
                 throw new \Exception("Error: Invalid Middleware. It must be Characters and PascalCase.", 1);
 
-            if (file_exists(app_path("Http/Middlewares/{$middleware}.php")))
+            if (file_exists(app_path("{$pre_middleware_path}/{$middleware}.php")))
                 throw new \Exception("Error: The Middleware is already created.", 1);
 
-            $output->writeln($this->makeTemplate($middleware) ? "Successfully created." : "File not created. Check the file path.");
+            $output->writeln($this->makeTemplate($sub_directories, $pre_middleware_path, $middleware) ? "Successfully created." : "File not created. Check the file path.");
         } catch (Exception $e) {
             $output->writeln($e->getMessage());
         }
@@ -49,20 +66,23 @@ class MiddlewareCommand extends BaseCommand
     /**
      * [Create middleware template]
      * @depends handle
+     * @param  [string] $sub_directories [sub directories of class]
+     * @param  [string] $pre_middleware_path [the pre string represent as folder before the file]
      * @param  [string] $middleware [middleware name]
      * @return [boolean]    [Return true if successfully creating file otherwise false]
      */
-    private function makeTemplate($middleware)
+    private function makeTemplate($sub_directories, $pre_middleware_path, $middleware)
     {
         $file = core_path("psr-4/Console/Commands/templates/middleware.php.dist");
         if (file_exists($file))
         {
             $template = strtr(file_get_contents($file), [
-                '{{namespace}}' => "{$this->namespace}",
+                '{{namespace}}' => $this->namespace,
+                '{{sub_directories}}' => $sub_directories,
                 '{{middleware}}' => $middleware
             ]);
 
-            $file_path = app_path("Http/Middlewares/{$middleware}.php");
+            $file_path = "{$pre_middleware_path}/{$middleware}.php";
 
             $file = fopen($file_path, "w");
             fwrite($file, $template);
