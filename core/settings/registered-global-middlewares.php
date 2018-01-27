395ea3d21a562080174ad7141013bd51e9db5172
rodz3rd <rodrigogalura3rd@gmail.com>
2018-01-27 10:13:57 +0800

    remove middleware for paginator

--- core/settings/registered-global-middlewares.php
+++ core/settings/registered-global-middlewares.php
@@ -24,6 +24,3 @@ $app->add(new M\RemoveTrailingSlash($container));
 
 # DetermineRouteBeforeAppMiddleware middleware
 $app->add(new M\DetermineRouteBeforeAppMiddleware($container));
-
-# Pagination middleware
-$app->add(new M\Pagination($container));
