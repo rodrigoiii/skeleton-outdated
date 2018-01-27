3b4d81a67ba40c4359d7970f3bb35cee7a17ac83
rodz3rd <rodrigogalura3rd@gmail.com>
2018-01-27 10:33:28 +0800

    remove DetermineRouteBeforeAppMiddleware

--- core/settings/registered-global-middlewares.php
+++ core/settings/registered-global-middlewares.php
@@ -21,6 +21,3 @@ $app->add(new M\SharedServer($container));
 
 # RemoveTrailingSlash middleware
 $app->add(new M\RemoveTrailingSlash($container));
-
-# DetermineRouteBeforeAppMiddleware middleware
-$app->add(new M\DetermineRouteBeforeAppMiddleware($container));
