20f8655b7cacfb63c04567341b32344e18ba1344
rodz3rd <rodrigogalura3rd@gmail.com>
2018-02-03 21:28:52 +0800

    add notification commands

--- config/special-commands.php
+++ config/special-commands.php
@@ -1,6 +1,12 @@
 <?php
 
+/**
+ * When you enable one of this, it will be available at commands
+ */
 return [
     'enable_rapid_authenticaion' => false,
-    'enable_quick_crud' => false
+
+    'enable_quick_crud' => false,
+
+    'enable_email_notification' => false
 ];
