96335678b057d03f97a7ee0f16505650b911a4f7
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-03-22 16:12:37 +0800

    use .env.testing in testing environment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,3 +2,8 @@
 
 // composer autoload
 require __DIR__ . "/../vendor/autoload.php";
+
+$dotenv = new \Dotenv\Dotenv(base_path(), ".env.testing");
+$dotenv->overload();
+
+$app = include __DIR__ . "/../bootstrap/app.php";
