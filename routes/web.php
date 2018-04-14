d330c2fa6a0a549f7229427127fbff3c1d613890
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-04-14 19:28:42 +0800

    reset to test snippet

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,3 @@
 <?php
 
 $app->get('/', "WelcomeController:index");
-
-(new \AuthSlim\AuthRoute(config('auth')))->routes($app, $container);
-
-$app->get('/authenticated-page', function ($request, $response) {
-    return $this->view->render($response, "auth/authenticated-home-page.twig");
-})
-->add(new App\Http\Middlewares\Auth\UserMiddleware($container))
-->setName('auth.authenticated-home-page');
