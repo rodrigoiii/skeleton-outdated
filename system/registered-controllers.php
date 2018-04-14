a036c5dc8db285b3b3ba8db5e45b6c418b66f917
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-04-14 19:26:58 +0800

    reset to test snippet

--- system/registered-controllers.php
+++ system/registered-controllers.php
@@ -6,33 +6,3 @@ $container['WelcomeController'] = function ($c)
     return new App\Http\Controllers\WelcomeController($c);
 };
 
-# Auth\AuthController
-$container['Auth\AuthController'] = function ($c)
-{
-    return new App\Http\Controllers\Auth\AuthController($c);
-};
-
-# Auth\RegisterController
-$container['Auth\RegisterController'] = function ($c)
-{
-    return new App\Http\Controllers\Auth\RegisterController($c);
-};
-
-# Auth\ForgotPasswordController
-$container['Auth\ForgotPasswordController'] = function ($c)
-{
-    return new App\Http\Controllers\Auth\ForgotPasswordController($c);
-};
-
-# Auth\ResetPasswordController
-$container['Auth\ResetPasswordController'] = function ($c)
-{
-    return new App\Http\Controllers\Auth\ResetPasswordController($c);
-};
-
-# Auth\AccountDetailController
-$container['Auth\AccountDetailController'] = function ($c)
-{
-    return new App\Http\Controllers\Auth\AccountDetailController($c);
-};
-
