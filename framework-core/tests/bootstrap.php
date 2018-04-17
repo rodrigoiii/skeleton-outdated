d6acbd9e325655b9267e0f68cbe3c80b5d7562fc
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-04-17 19:16:43 +0800

    remove system-core in this project

--- framework-core/tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// composer autoload
-require "../vendor/autoload.php";
-
-$dotenv = new \Dotenv\Dotenv(base_path(), ".env.testing");
-$dotenv->overload();
-
-$app = include base_path("bootstrap/app.php");
