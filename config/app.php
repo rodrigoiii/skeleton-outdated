d240fc9d16cda8e861256d40f062f0d37cabef65
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-04-27 16:57:40 +0800

    remove testing environment

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 return [
     'name'        => _env('APP_NAME', "App Name"),
     'namespace'   => _env('APP_NAMESPACE', "App"),
-    'environment' => _env('APP_ENV', "development"), // Options: development, production, testing
+    'environment' => _env('APP_ENV', "development"), // Options: development, production
     'key'         => _env('APP_KEY'),
 
     # default timezone
