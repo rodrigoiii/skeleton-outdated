8462ff283f2aab93de96fcf3486d0eb3180a9264
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-04-27 17:56:34 +0800

    return the code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
 // composer autoload
-require __DIR__ . "/../../vendor/autoload.php";
+require __DIR__ . "/../vendor/autoload.php";
 
 $dotenv = new \Dotenv\Dotenv(base_path(), ".env.testing");
 $dotenv->overload();
 
-$app = include __DIR__ . "/../../bootstrap/app.php";
+$app = include __DIR__ . "/../bootstrap/app.php";
