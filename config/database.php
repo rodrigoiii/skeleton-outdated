a658c4d8853737a42f4666b09657eabb894f60a9
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-07-13 11:50:01 +0800

    set strict to true

--- config/database.php
+++ config/database.php
@@ -14,7 +14,7 @@ return [
             'collation' => "utf8_unicode_ci",
             'prefix'    => "",
 
-            'strict' => false,
+            'strict' => true,
             'engine' => null
         ]
     ]
