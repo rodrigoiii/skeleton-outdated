76a0e82887ad64e2db9442d66a6525f19e72edcb
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-07-13 17:21:39 +0800

    change WEB_MODE to APP_MODE

--- config/app.php
+++ config/app.php
@@ -5,7 +5,7 @@ return [
     'namespace'   => _env('APP_NAMESPACE', "App"),
     'environment' => _env('APP_ENV', "development"),
     'key'         => _env('APP_KEY'),
-    'app_mode'    => filter_var(_env('UP', false), FILTER_VALIDATE_BOOLEAN),
+    'mode'        => filter_var(_env('APP_MODE', false), FILTER_VALIDATE_BOOLEAN),
     'use_dist'    => filter_var(_env('USE_DIST', false), FILTER_VALIDATE_BOOLEAN),
     'debug'       => filter_var(_env('DEBUG_ON', false), FILTER_VALIDATE_BOOLEAN),
 
