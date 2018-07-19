48c0c6351dcf6b79503393123f4bfa1bfe2960bc
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-07-19 09:19:32 +0800

    update outdated csrf-helper.js

--- resources/assets/js/built-in/csrf-helper.js
+++ resources/assets/js/built-in/csrf-helper.js
@@ -1,32 +1,34 @@
-$(document).ready(function() {
-    $.ajaxSetup({
-        complete: function (jqXHR)
-        {
-            var new_token = jqXHR.getResponseHeader('X-CSRF-TOKEN');
-            $('meta[name="_token"]').attr('content', new_token);
-        }
-    });
-});
+var CsrfHelper = {
+    init: function() {
+        $.ajaxSetup({
+            complete: function (jqXHR)
+            {
+                var new_token = jqXHR.getResponseHeader('X-CSRF-TOKEN');
+                $('meta[name="_token"]').attr('content', new_token);
+            }
+        });
+    },
 
-/**
- * The framework use slim csrf(cross site request forge) by default.
- * Use this function to use POST request.
- *
- * @param  json data
- * @return json data with csrf token
- */
-function postWithToken(data)
-{
-    var csrf = {};
+    /**
+     * Helper function to use post request via ajax.
+     *
+     * @param  json data
+     * @return json data with csrf token
+     */
+    postWithToken: function(data) {
+        var csrf = {};
 
-    if ($('meta[name="_token"]').length > 0)
-    {
-        var token = JSON.parse($('meta[name="_token"]').attr('content'));
-        for (var key in token)
+        if ($('meta[name="_token"]').length > 0)
         {
-            csrf[key] = token[key];
+            var token = JSON.parse($('meta[name="_token"]').attr('content'));
+            for (var key in token)
+            {
+                csrf[key] = token[key];
+            }
         }
+
+        return $.extend(csrf, data);
     }
+};
 
-    return $.extend(csrf, data);
-}
+module.exports = CsrfHelper;
