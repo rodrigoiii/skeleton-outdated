58b620982e4e3a103b116f4ce20e655fc593aa9c
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-11-04 08:24:59 +0800

    add routes for registration and reset password

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,11 @@ $app->get('/', ["WelcomeController", "index"]);
 $app->group('/auth', function() {
     $this->get('/login', ["LoginController", "getLogin"])->setName('auth.login');
     $this->post('/login', ["LoginController", "postLogin"]);
+
+    // registration /register/verify?token=
+    $this->post('/register/verify', ["RegisterController", "verify"]);
+
+    // reset password /reset-password?token=
+    $this->get('/register/reset-password', ["ResetPasswordController", "getResetPassword"]);
+    $this->post('/register/reset-password', ["ResetPasswordController", "postResetPassword"]);
 });
