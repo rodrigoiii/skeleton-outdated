bc5cc60da7476cd582c47fead3e96be2fb019a24
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-11-12 17:53:03 +0800

    add migrations table

---
+++ db/migrations/20181112092336_create_table_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateTableUsers extends AbstractMigration
+{
+    /**
+     * [up description]
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $table = $this->table('users')
+            ->addColumn('first_name', 'string', ['limit' => 50])
+            ->addColumn('last_name', 'string', ['limit' => 50])
+            ->addColumn('email', 'string', ['limit' => 50])
+            ->addColumn('password', 'string', ['limit' => 60])
+            ->addIndex('email', ['unique' => true])
+            ->addTimestamps();
+
+        $table->create();
+    }
+
+    /**
+     * [down description]
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table_exist = $this->hasTable('users');
+        if ($table_exist)
+        {
+            $this->dropTable('users');
+        }
+    }
+}
