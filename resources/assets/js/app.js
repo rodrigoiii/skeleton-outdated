3708dd4c4ce7680ada97359aa687f9ccc86aa600
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-11-19 15:27:31 +0800

    add preview for image

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,3 +6,19 @@ if (typeof(jQuery) === "undefined") {
 require("bootstrap/js/transition");
 require("bootstrap/js/collapse");
 require("bootstrap/js/dropdown");
+
+function readURL(input, output_el) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.readAsDataURL(input.files[0]);
+
+        reader.onload = function(e) {
+            output_el.setAttribute('src', e.target.result);
+        };
+    } else {
+        output_el.setAttribute('src', "");
+    }
+}
+
+window.readURL = readURL;
---
+++ resources/assets/js/functions.js
@@ -0,0 +1,15 @@
+/*
+
+function readURL(input, output_el) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.readAsDataURL(input.files[0]);
+
+        reader.onload = function(e) {
+            output_el.setAttribute(e.target.result);
+        };
+    }
+}
+
+*/
