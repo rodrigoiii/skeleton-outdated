a35f5f25c18f44cf808ae140196d8c4447508ff4
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-11-19 17:03:15 +0800

    to be continue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,18 +7,33 @@ require("bootstrap/js/transition");
 require("bootstrap/js/collapse");
 require("bootstrap/js/dropdown");
 
-function readURL(input, output_el) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+function quickImageLoad(input, output_el) {
+    var reader = new FileReader();
 
-        reader.readAsDataURL(input.files[0]);
+    if (typeof(input.files) !== "undefined") {
+        if (input.files.length === 1) { // single file
+            if (/^image\/./g.test(input.files[0].type)) { // valid image
 
-        reader.onload = function(e) {
-            output_el.setAttribute('src', e.target.result);
-        };
-    } else {
-        output_el.setAttribute('src', "");
+            } else {
+                output_el.setAttribute('src', "");
+            }
+
+            // if (input.files && input.files[0]) {
+            //     reader.readAsDataURL(input.files[0]);
+
+            //     reader.onload = function(e) {
+            //         output_el.setAttribute('src', e.target.result);
+            //     };
+            // } else {
+            //     output_el.setAttribute('src', "");
+            // }
+        } else if (input.files.length > 1) { // multiple files
+
+        }
     }
+
+
+    return null;
 }
 
-window.readURL = readURL;
+window.quickImageLoad = quickImageLoad;
