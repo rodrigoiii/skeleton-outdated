e0cb84ae15b97e839debb6b78c8cd5b0de4cfa0a
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-11-23 17:39:15 +0800

    remove duplicate migration

--- db/migrations/20181112092336_create_table_users.php
+++ db/migrations/20181112092336_create_table_users.php
@@ -12,6 +12,7 @@ class CreateTableUsers extends AbstractMigration
     public function up()
     {
         $table = $this->table('users')
+            ->addColumn('picture', 'string', ['limit' => 13]) // uniqid without more_entropy
             ->addColumn('first_name', 'string', ['limit' => 50])
             ->addColumn('last_name', 'string', ['limit' => 50])
             ->addColumn('email', 'string', ['limit' => 50])
